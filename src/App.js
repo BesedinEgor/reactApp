d45cff943c67d9d602a04360cade514102261aba
BesedinEgor <74902393+BesedinEgor@users.noreply.github.com>
2022-09-26 19:35:57 +0300

    #4 добавление ключей для объектов, стилизация элементов компонента через пары ключ-значение (#8)

--- src/App.js
+++ src/App.js
@@ -8,33 +8,43 @@ import GeneratingElements from "./components/generatingElements/GeneratingElemen
 
 const OBJECTS = [
   {
+    color: "#FBCEB1",
     value: 4.1,
   },
   {
+    color: "#B5B8B1",
     value: "Method",
   },
   {
+    color: "#7FFFD4",
     value: 123,
   },
   {
+    color: "#78DBE2",
     value: "Component",
   },
   {
+    color: "#E32636",
     value: "Class",
   },
   {
+    color: "#AB274F",
     value: 2.5,
   },
   {
+    color: "#9966CC",
     value: "Operator",
   },
   {
+    color: "#44944A",
     value: 55,
   },
   {
+    color: "#CCCCFF",
     value: 432,
   },
   {
+    color: "#77DDE7",
     value: 364,
   }
 ];
