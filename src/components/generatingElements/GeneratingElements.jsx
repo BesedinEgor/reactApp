c5b658ecdbf460ef2d7509adef05b933be14f889
BesedinEgor <74902393+BesedinEgor@users.noreply.github.com>
2022-09-29 21:36:14 +0300

    #5 условия для копии основного массива, работа с методами map & filter (#14)

--- src/components/generatingElements/GeneratingElements.jsx
+++ src/components/generatingElements/GeneratingElements.jsx
@@ -3,9 +3,39 @@ import React from "react";
 export const GeneratingElements = (props) => {
   return (
     <section className="elements">
-      {props.obj.map(el => <div className="square" style={{backgroundColor:el.color}}>{el.value}</div>)}
+      {props.cloneObjects
+        .filter((el) => el.value !== String(el.value))
+        .map((el) => {
+          if (el.value > 5) {
+            return <div
+              className="square"
+              key={el.value}
+              style={{ backgroundColor: "#44944A" }}
+            >
+              {el.value}
+            </div>;
+          } else if (el.value <= 5 && el.value >= 0) {
+            return <div
+              className="square"
+              key={el.value}
+              style={{ backgroundColor: "#FFCF48" }}
+            >
+              {el.value}
+            </div>;
+          } else if (el.value < 0) {
+            return <div
+              className="square"
+              key={el.value}
+              style={{ backgroundColor: "#B00000" }}
+            >
+              {el.value}
+            </div>;
+          } else {
+            return null;
+          }
+        })}
     </section>
-  )
+  );
 };
 
 export default GeneratingElements;
