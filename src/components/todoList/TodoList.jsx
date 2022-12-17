986e3b2986bccc3a49ddd0a7f246c0586e9fe2aa
Egor Besedin <Besedinegor@mail.ru>
2022-12-17 18:50:21 +0300

    #35 добавление элемента списка через пропс улучшено, чекбоксы

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,13 +1,12 @@
 import { React } from 'react';
 import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
-import { MantineProvider, Button, TextInput } from '@mantine/core';
+import { MantineProvider, Button, TextInput, Checkbox } from '@mantine/core';
 
 const TodoList = ({ items }) => {
   TodoList.propTypes = {
     items: PropTypes.object.isRequired,
   };
-  console.log(items);
   return (
     <MantineProvider>
       <h1
@@ -19,25 +18,34 @@ const TodoList = ({ items }) => {
       <Link className="back" to={'/'}>
         Back
       </Link>
-      <form className="todo">
-        <TextInput
-          placeholder="Your name"
-          description="Поле для ввода текста"
-          radius="md"
-        />
-        <Button
-          className="todo__button"
-          type="submit"
-          variant="gradient"
-          gradient={{ from: 'teal', to: 'lime', deg: 105 }}
-        >
-          Показать
-        </Button>
-      </form>
-      <ul className="todoItems">
-        <li className="todoItems__item">{items[0].title}</li>
-        <li className="todoItems__item">{items[1].title}</li>
-      </ul>
+      <div className="todo">
+        <form className="todo__form">
+          <TextInput
+            placeholder="Tasks"
+            description="Поле для ввода текста"
+            radius="md"
+          />
+          <Button
+            className="todo__button"
+            type="submit"
+            variant="gradient"
+            gradient={{ from: 'teal', to: 'lime', deg: 105 }}
+          >
+            Показать
+          </Button>
+        </form>
+        <div className="todoList">
+          {items.map((listItem) => {
+            return (
+              <Checkbox
+                className="todoList__checkbox"
+                label={listItem.title}
+                key={items.id}
+              />
+            );
+          })}
+        </div>
+      </div>
     </MantineProvider>
   );
 };
