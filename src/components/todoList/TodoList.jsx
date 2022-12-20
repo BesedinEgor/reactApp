b86dcebdf1af5373008e032856e4e7a0c4562e22
Egor Besedin <Besedinegor@mail.ru>
2022-12-20 23:23:55 +0300

    #35 реализован вывод в консоль значений инпута по нажатию на кнопку

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,12 +1,19 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 import { MantineProvider, Button, TextInput } from '@mantine/core';
 import { OutputTasks } from './OutputTasks';
 
 const TodoList = ({ items }) => {
+  const [output, setOutput] = useState('hi');
+
   TodoList.propTypes = {
     items: PropTypes.array.isRequired,
+  };
+
+  const btnHundler = (e) => {
+    e.preventDefault();
+    console.log(output);
   };
   return (
     <MantineProvider>
@@ -26,12 +33,17 @@ const TodoList = ({ items }) => {
             placeholder="Tasks"
             description="Поле для ввода текста"
             radius="md"
+            onChange={(event) => {
+              setOutput(event.currentTarget.value);
+            }}
+            defaultValue={output}
           />
           <Button
             className="todo__button"
             type="submit"
             variant="gradient"
             gradient={{ from: 'teal', to: 'lime', deg: 105 }}
+            onClick={btnHundler}
           >
             Показать
           </Button>
