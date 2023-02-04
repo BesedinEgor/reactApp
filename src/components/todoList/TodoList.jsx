0cee59eec2efa124695b6e004e2eeb023c90a093
Egor <besedinegor@mail.ru>
2023-02-04 20:15:20 +0300

    добавление нового элемента списка с title из input  после нажатия на кнопку

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -2,23 +2,25 @@ import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 import { MantineProvider, Button, TextInput, Checkbox } from '@mantine/core';
-// import { OutputTasks} from './OutputTasks';
 import { randomId } from '../../js';
 
 const TodoList = ({ items }) => {
-  const [output, setOutput] = useState({});
-  const [copyItems, setCopyItems] = useState(items);
+  const [output, setOutput] = useState(''); //input
+  const [list, setList] = useState(items); //list items
 
   TodoList.propTypes = {
     items: PropTypes.array.isRequired,
   };
 
-  const btnHundler = (e) => {
-    setCopyItems([...copyItems, output]);
-    e.preventDefault();
-    console.log(output);
-    console.log(items);
-    console.log(copyItems);
+  const handlerInput = (event) => {
+    setOutput(event.target.value);
+  };
+
+  const handlerButton = (event) => {
+    if (output.length > 0) {
+      setList([...list, {id: randomId(), title: output} ])
+      event.preventDefault();
+    }
   };
   return (
     <MantineProvider>
@@ -38,23 +40,21 @@ const TodoList = ({ items }) => {
             placeholder="Tasks"
             description="Поле для ввода текста"
             radius="md"
-            value={output.title}
-            onChange={(event) => {
-              setOutput({id: randomId(), title: event.target.value});
-            }}
+            value={items.title}
+            onChange={handlerInput}
           />
           <Button
             className="todo__button"
             type="submit"
             variant="gradient"
             gradient={{ from: 'teal', to: 'lime', deg: 105 }}
-            onClick={btnHundler}
+            onClick={handlerButton}
           >
             Показать
           </Button>
         </form>
         <div className="todoList">
-          {items.map((listItem) => {
+          {list.map((listItem) => {
             return (
               <div key={listItem.id}>
                 <Checkbox
